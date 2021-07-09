91ba34add82e110eb4dd3905b88bdd0f830267a4
Rog3rSm1th <r0g3r5@protonmail.com>
2021-07-09 20:13:52 +0200

    Fix config and ressources loading

--- profil3r/main.py
+++ profil3r/main.py
@@ -1,5 +1,6 @@
 from profil3r.app.core import Core
+import os 
 
 def main():
-    CONFIG = './profil3r/config/config.json'
+    CONFIG = '{}/config/config.json'.format(os.path.dirname(os.path.realpath(__file__)))
     profil3r = Core(CONFIG).run()
