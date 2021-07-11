79fd73fa709ac6f09b0751f27ffe288433e6c6e5
Rog3rSm1th <r0g3r5@protonmail.com>
2021-07-11 20:44:28 +0200

    Add PyPi + minor fixes

--- profil3r/app/core/services/_programming.py
+++ profil3r/app/core/services/_programming.py
@@ -1,6 +1,7 @@
 from profil3r.app.modules.programming.github import Github
 from profil3r.app.modules.programming.pastebin import Pastebin
 from profil3r.app.modules.programming.replit import Replit
+from profil3r.app.modules.programming.pypi import PyPi
 
 # Github
 def github(self):
@@ -19,3 +20,9 @@ def replit(self):
     self.result["replit"] = Replit(self.config, self.permutations_list).search() 
     # print results
     self.print_results("replit")
+
+# PyPi
+def pypi(self):
+    self.result["pypi"] = PyPi(self.config, self.permutations_list).search() 
+    # print results
+    self.print_results("pypi")
