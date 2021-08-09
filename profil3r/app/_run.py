265d0fd9a84dde4dc4bd968c4a7a4a5c59fd966f
Rog3rSm1th <r0g3r5@protonmail.com>
2021-08-09 16:55:28 +0200

    Exit and error message if no service is selected

--- profil3r/app/_run.py
+++ profil3r/app/_run.py
@@ -11,17 +11,21 @@ def run(self):
     self.menu()
     self.get_permutations()
 
-    # Number of permutations to test per service
-    print(Colors.BOLD + "[+]" + Colors.ENDC + " {} permutations to test for each service, you can reduce this number by selecting less options if it takes too long".format(len(self.permutations_list)))
-
     modules = self.get_report_modules()
 
-    print("\n" + "Profil3r will search : \n " + Colors.BOLD + "[+] " + Colors.ENDC +  "{} \n".format(str('\n ' + Colors.BOLD + "[+] " + Colors.ENDC).join(modules)))
+    if modules:
+        # Number of permutations to test per service
+        print(Colors.BOLD + "[+]" + Colors.ENDC + " {} permutations to test for each service, you can reduce this number by selecting less options if it takes too long".format(len(self.permutations_list)))
 
-    for module in modules:
-        thread = threading.Thread(target=self.modules[module]["method"])
-        thread.start()
-        thread.join()
-    
+        # List of services that profil3r will search
+        print("\n" + "Profil3r will search : \n " + Colors.BOLD + "[+] " + Colors.ENDC +  "{} \n".format(str('\n ' + Colors.BOLD + "[+] " + Colors.ENDC).join(modules)))
+
+        for module in modules:
+            thread = threading.Thread(target=self.modules[module]["method"])
+            thread.start()
+            thread.join()
+    else:
+        print("\nNo service selected.")
+        
     if self.report_path:
         self.generate_report()
